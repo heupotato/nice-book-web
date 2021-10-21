b12bd41f27ed32f195cfabee55ce67371c0ee467
LoanThai <thloanth.dut@gmail.com>
2021-10-21 23:34:39 +0700

    remove unused import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { ROUTES, PRIVATE_ROUTES_ADMIN, PRIVATE_ROUTES_MANAGER, PRIVATE_ROUTES_USER } from './routes/routes';
 import './App.css';
-import SignUp from './pages/signup';
 
 function App() {
   return (
