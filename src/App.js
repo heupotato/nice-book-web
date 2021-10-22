356a0436d97837b439768354bf102bce872f0c2e
LoanThai <thloanth.dut@gmail.com>
2021-10-23 01:22:15 +0700

    Update routes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import { ROUTES, PRIVATE_ROUTES_ADMIN, PRIVATE_ROUTES_MANAGER, PRIVATE_ROUTES_US
 import './App.css';
 import Footer from './components/footer';
 import Header from './components/headerUser';
+import LocalStorageService from './services/localStorage';
 
 function App() {
+
+  const isLoggedIn = () => {
+	  return LocalStorageService.token ===""; 
+  }
+ 
   return (
     <div className="app-container">
       <Router>
@@ -13,6 +19,9 @@ function App() {
           {
             showRoutesPublic(ROUTES)
           }
+		  {
+			showRoutesPrivateUser(PRIVATE_ROUTES_USER, isLoggedIn())
+		  }
         </Switch>
 	    <Footer/>
       </Router>
@@ -46,6 +55,26 @@ const showRoutesPrivateManager = (routes, isLoggedIn, role) => {
 				path={route.path}
 				exact
 				render={props => (isLoggedIn && role === 2) ? <route.main {...props} /> :
+					<Redirect to={{
+						pathname: '',
+						state: { from: props.location }
+					}} />}
+			/>)
+
+		})
+	}
+	return result;
+}
+
+const  showRoutesPrivateUser = (routes, isLoggedIn, role) => {
+	var result = null;
+	if (routes.length > 0) {
+		result = routes.map((route, index) => {
+			return (<Route
+				key={index}
+				path={route.path}
+				exact
+				render={props => (isLoggedIn) ? <route.main {...props} /> :
 					<Redirect to={{
 						pathname: '',
 						state: { from: props.location }
