0002536304f81896a84338fc56263076a1cf3f79
ChauWei200220 <65481625+ChauWei200220@users.noreply.github.com>
2021-10-23 02:40:47 +0700

    Update auth service

--- src/api-services/auth-service.js
+++ src/api-services/auth-service.js
@@ -3,26 +3,67 @@ import axios from "axios";
 const API_URL = "http://localhost:8080/api/v1/auth/";
 
 class AuthService {
-    login (email, password) {
-        return axios.post(API_URL + "login", {
+    async login (email, password) {
+        return await axios.post(API_URL + "login", {
             email,
             password
         }).then(res => {
-            if (res.data.token) {
-                localStorage.setItem("user", JSON.stringify(res.data));
-                localStorage['email'] = res.data.email;
-                localStorage['password'] = res.data.password;
-                localStorage['accessToken'] = res.data.token;
+                if (res.data) {
+                    localStorage.setItem("user", JSON.stringify(res.data.body));
+                    localStorage['email'] = res.data.body.user.email;
+                    localStorage['accessToken'] = res.data.body.token;
+                }
+                return res.data;
+            }, error => {
+                return error.response.data;
             }
-            return res.data
+        )
+    }
+
+    async signup (email, userName, password) {
+        return await axios.post(API_URL + "register", {
+            email,
+            //NEED_TODO: userName -> username
+            userName,
+            password
+        }).then(res => {
+                return res.data;
+            }, error => {
+                return error.response.data;
+            }
+        )
+    }
+
+    async forgotPassword (email) {
+        return await axios.post(API_URL + "forgot-password", {
+            email
+        }).then(res => {
+            return res.data;
+        }, error => {
+            return error.response.data;
         })
     }
 
-    signup (email, userName, password) {
-        return axios.post(API_URL + "register", {
+    async resetPassword (email, code, newPassword) {
+        return await axios.put(API_URL + "reset-password", {
             email,
-            userName,
-            password
+            code,
+            newPassword
+        }).then(res => {
+                return res.data;
+            }, error => {
+                return error.response.data;
+            }
+        )
+    }
+
+    async resendPasswordCode (email) {
+        return await axios.put(API_URL + "resend-password-code", {
+            email
+        }).then(res => {
+            return res.data;
+        }, error => {
+            return error.response.data;
         })
     }
 
@@ -33,13 +74,28 @@ class AuthService {
         localStorage.removeItem("accessToken");
     }
 
-    verify_account (email, code) {
-        return axios.put(API_URL + "verify", {
+    async verifyCode (email, code) {
+        return await axios.put(API_URL + "verify", {
             email,
             code
-        })
+        }).then(res => {
+                return res.data;
+            }, error => {
+                return error.response.data;
+            }
+        )
+    }
+
+    async resendCode (email) {
+        return await axios.put(API_URL + "resend-verify-code", {
+            email
+        }).then(res => {
+                return res.data;
+            }, error => {
+                return error.response.data;
+            }
+        )
     }
 }
 
 export default new AuthService();
-
