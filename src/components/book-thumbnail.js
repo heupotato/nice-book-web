b775d54e6aa1746fe63efdeddcd10275c0290666
LoanThai <thloanth.dut@gmail.com>
2021-10-30 00:48:11 +0700

    create thumbnail

---
+++ src/components/book-thumbnail.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+function BookThumbnail(prop){
+    return(
+        <div className='book-thumbnail-container'>
+            <div className="card book-card " >
+                <img className="card-img-top" src={prop.image} alt="Card image" style={{width:'100%'}}/>
+                <div className="card-body">
+                    <h4 className="card-title">{prop.bookName}</h4>
+                    <p className="card-text">Author: {prop.author}</p>
+                    <Link to="#" className="btn btn-primary">View Book</Link>
+                </div>
+            </div>
+        </div>
+    ); 
+}
+
+export default BookThumbnail; 
