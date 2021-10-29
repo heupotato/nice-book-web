09545993aeb144ed2b21abb2baf2b7475469dbdf
LoanThai <thloanth.dut@gmail.com>
2021-10-30 00:48:19 +0700

    add service

---
+++ src/api-services/book-list.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import authHeader from "./auth-header";
+
+const API_URL = "http://localhost:8080/api/v1/users/";
+
+class BookListService{
+    getReadings(userID){
+        return axios.get(API_URL + userID + "/books/reading"); 
+        //if header is necessary, uncomment this code
+        //return axios.get(API_URL + userID + "/books/reading, {headers: authHeader()}"); 
+    }
+
+    getFavourites(userID){
+        return axios.get(API_URL + userID + "/books/favorites"); 
+    }
+
+    getReadLater(userID){
+        return axios.get(API_URL + userID + "/books/readAfter"); 
+    }
+}
+
+export default new BookListService; 
