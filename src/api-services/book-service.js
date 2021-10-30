e06a87b50c32f21702e95a2327fd95ba703e92d0
ChauWei200220 <65481625+ChauWei200220@users.noreply.github.com>
2021-10-31 02:04:54 +0700

    Upgrade header search

--- src/api-services/book-service.js
+++ src/api-services/book-service.js
@@ -8,6 +8,16 @@ class BookService {
         //return await axios.get(API_URL + id);
         return await axios.get(API_URL + id, {headers: authHeader()});
     }
+
+    async searchBook (filter) {
+        console.log(JSON.stringify(filter))
+        return await axios.get("http://localhost:8080/api/v1/books" + "?filter=" + JSON.stringify(filter) ,{headers: authHeader()}
+        ).then(res => {
+            return res.data.docs[0];
+        }, error => {
+            return error.response.data;
+        })
+    }
 }
 
 export default new BookService;
