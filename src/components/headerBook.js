44fde3d37c86169497826d92fccfde8d71d32efd
ChauWei200220 <65481625+ChauWei200220@users.noreply.github.com>
2021-10-31 16:23:59 +0700

    Add dropdown categories

--- src/components/headerBook.js
+++ src/components/headerBook.js
@@ -1,31 +1,33 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import Link from 'react-dom';
 import BookService from "../api-services/book-service";
-function HeaderBook() {
 
-    const [categoriesList, setCategories] = useState(); 
+function HeaderBook() {
+    const [categoriesList, setCategories] = useState();
 
     useEffect(() => {
-        BookService.getAllGenres().then(response => {
-            let genres = response.data; 
-            let categorieDropDown = genres.map((genre)=> {
+        BookService.getAllGenres().then(res => {
+            let genres = res.data;
+            let categoriesDropDown = genres.map((genre) => {
                 <div className="dropdown-content">
-                    <Link to={"/category/" + genre}>{genre}</Link>
+                    <Link to={"/categories" + genre}>{genre}</Link>
                 </div>
             })
-            setCategories(categorieDropDown); 
-        })
-        .catch(err => console.log(err))
-    }, [])
-    
+            setCategories(categoriesDropDown);
+        }).catch(err => console.log(err))
+    })
+
     return(
         <header className="header-book">
             <button className="btn-header">Discover</button> &nbsp;
             <button className="btn-header">Top Books</button> &nbsp;
             <button className="btn-header">New Release</button> &nbsp;
             <button className="btn-header">Recommended</button> &nbsp;
-            <div style={{display: 'flex', flexDirection: 'row'}}>
-                <button style={{marginTop: '20px', background: 'white', border: 'none', fontWeight: '600', marginBottom: '20px'}}>Categories</button>
+            <div className="dropdown" style={{display: 'flex', flexDirection: 'row'}}>
+                <div style={{marginTop: '20px', background: 'white', border: 'none', fontWeight: '600', marginBottom: '20px'}}>Categories</div>
+                <div className="dropdown-content">
+                    {categoriesList}
+                </div>
                 <i class="fas fa-angle-down" style={{marginTop: '25px', marginLeft: '10px'}}></i>
             </div>
         </header>
