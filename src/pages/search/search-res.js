59fa4cd2f0c84e6b2dbe2840bca75175a2463ae0
LoanThai <thloanth.dut@gmail.com>
2021-10-31 17:56:26 +0700

    Search page

--- src/pages/search/search-res.js
+++ src/pages/search/search-res.js
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import { useLocation } from "react-router";
+import { useEffect } from "react/cjs/react.development";
+import BookThumbnail from "../../components/book-thumbnail";
 function SearchResult(){
     const location = useLocation(); 
     console.log("Search Page"); 
-    console.log(location.state); 
-    return(
-        <div>
+    // console.log(location.state); 
 
+    const [searchRes, setSearchRes] = useState([]); 
+
+    useEffect(() => {
+        // var res = location.state; 
+        var res = []
+        let listRes = res.map((book) => {
+            <BookThumbnail image={book.title} author={book.author} image={book.image}></BookThumbnail>
+        })
+        setSearchRes(listRes)
+    }, []); 
+
+    if (searchRes.length == 0 )
+    return(
+        <div style={{height:'100vh'}}>
+            <img className="img-not-found" src='../images/book-not-found.png'></img>
+        </div>
+    ); 
+    else return(
+        <div className="book-row">
+            {searchRes}
         </div>
     ); 
 }
