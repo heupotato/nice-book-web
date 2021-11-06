60c915928ae1954fdb2528128634e679ac1c13c6
LoanThai <thloanth.dut@gmail.com>
2021-11-06 11:43:43 +0700

    Update book-thumbnail.js

--- src/components/book-thumbnail.js
+++ src/components/book-thumbnail.js
@@ -4,7 +4,7 @@ function BookThumbnail(prop){
         <div className='book-thumbnail-container'>
             <div className="card book-card " >
                 <img className="card-img-top" src={prop.image} alt="Card image" style={{width:'100%'}}/>
-                <div className="card-body">
+                <div className="card-body" style={{whiteSpace:'nowrap'}}>
                     <h4 className="card-title">{prop.bookName}</h4>
                     <p className="card-text email-row ">Author: {prop.author}</p>
                     <Link to={"/books/" + prop.id} className="btn btn-primary">View Book</Link>
