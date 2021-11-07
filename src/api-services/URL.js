d3db7887de1d1176cd3293d187b28a7ca2a4ebd0
LoanThai <thloanth.dut@gmail.com>
2021-11-07 10:27:36 +0700

    Update URL.js

--- src/api-services/URL.js
+++ src/api-services/URL.js
@@ -1,5 +1,5 @@
 class URLService{
-    static NGROK_URL = 'https://61b7-2001-ee0-4c7e-4580-7e6-a4b-308a-614d.ngrok.io/api/v1/'; 
+    static NGROK_URL = 'https://ff73-2001-ee0-4c79-1ce0-252c-1923-aeb1-c8d9.ngrok.io/api/v1/'; 
     static LOCALHOST_URL = 'http://localhost:8080/api/v1/'; 
 }
 
