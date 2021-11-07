26302699695b8cd31a5813185b06810178061778
ChauWei200220 <65481625+ChauWei200220@users.noreply.github.com>
2021-11-07 20:14:19 +0700

    change url ngrok

--- src/api-services/URL.js
+++ src/api-services/URL.js
@@ -1,5 +1,5 @@
 class URLService{
-    static NGROK_URL = 'https://ff73-2001-ee0-4c79-1ce0-252c-1923-aeb1-c8d9.ngrok.io/api/v1/'; 
+    static NGROK_URL = 'https://bc25-2001-ee0-4c79-1ce0-8c1e-f67d-a0aa-8fc4.ngrok.io/api/v1/'; 
     static LOCALHOST_URL = 'http://localhost:8080/api/v1/'; 
 }
 
