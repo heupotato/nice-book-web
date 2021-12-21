e8e9f17f3fa84f19a1a04d4e40939a80baa49cb9
ChauWei200220 <65481625+ChauWei200220@users.noreply.github.com>
2021-12-21 21:12:20 +0700

    change url be

--- src/api-services/auth-service.js
+++ src/api-services/auth-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/v1/auth/";
+const API_URL = "https://nice-book.herokuapp.com/api/v1/auth/";
 
 class AuthService {
     async login (email, password) {
