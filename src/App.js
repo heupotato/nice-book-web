98b585a48f487ed22ca5909d282cc4ba420e1238
lvl162 <loithui162@gmail.com>
2021-12-21 22:47:20 +0700

    ready for gh pages deployment

--- src/App.js
+++ src/App.js
@@ -1,91 +1,114 @@
-import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import { ROUTES, PRIVATE_ROUTES_ADMIN, PRIVATE_ROUTES_MANAGER, PRIVATE_ROUTES_USER } from './routes/routes';
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+import {
+  ROUTES,
+  PRIVATE_ROUTES_ADMIN,
+  PRIVATE_ROUTES_MANAGER,
+  PRIVATE_ROUTES_USER,
+} from './routes/routes';
 import './App.css';
 import Footer from './components/footer';
 import Header from './components/header';
-import HeaderUser from './components/headerUser'
+import HeaderUser from './components/headerUser';
 import LocalStorageService from './services/localStorage';
 
 function App() {
-
   const isLoggedIn = () => {
-	//   return true
-	  return (LocalStorageService.token !=="" &&  LocalStorageService.token !==null );
-  }
+    //   return true
+    return (
+      LocalStorageService.token !== '' && LocalStorageService.token !== null
+    );
+  };
 
   return (
-    <div className="app-container">
-      <Router>
-	    {isLoggedIn() ? <HeaderUser/> : <Header/>}
+    <div className='app-container'>
+      <Router basename='/'>
+        {isLoggedIn() ? <HeaderUser /> : <Header />}
         <Switch>
-          {
-            showRoutesPublic(ROUTES)
-          }
-		  {
-			showRoutesPrivateUser(PRIVATE_ROUTES_USER, isLoggedIn())
-		  }
+          {showRoutesPublic(ROUTES)}
+          {showRoutesPrivateUser(PRIVATE_ROUTES_USER, isLoggedIn())}
         </Switch>
-	    <Footer/>
+        <Footer />
       </Router>
-
     </div>
   );
 }
 
 const showRoutesPublic = (routes) => {
-	var result = null;
-	if (routes.length > 0) {
-		result = routes.map((route, index) => {
-			return (<Route
-				key={index}
-				path={route.path}
-				exact={route.exact}
-				render={props => <route.main {...props} />}
-			/>)
-
-		})
-	}
-	return result;
-}
+  var result = null;
+  if (routes.length > 0) {
+    result = routes.map((route, index) => {
+      return (
+        <Route
+          key={index}
+          path={route.path}
+          exact={route.exact}
+          render={(props) => <route.main {...props} />}
+        />
+      );
+    });
+  }
+  return result;
+};
 
 const showRoutesPrivateManager = (routes, isLoggedIn, role) => {
-	var result = null;
-	if (routes.length > 0) {
-		result = routes.map((route, index) => {
-			return (<Route
-				key={index}
-				path={route.path}
-				exact
-				render={props => (isLoggedIn && role === 2) ? <route.main {...props} /> :
-					<Redirect to={{
-						pathname: '',
-						state: { from: props.location }
-					}} />}
-			/>)
+  var result = null;
+  if (routes.length > 0) {
+    result = routes.map((route, index) => {
+      return (
+        <Route
+          key={index}
+          path={route.path}
+          exact
+          render={(props) =>
+            isLoggedIn && role === 2 ? (
+              <route.main {...props} />
+            ) : (
+              <Redirect
+                to={{
+                  pathname: '',
+                  state: { from: props.location },
+                }}
+              />
+            )
+          }
+        />
+      );
+    });
+  }
+  return result;
+};
 
-		})
-	}
-	return result;
-}
-
-const  showRoutesPrivateUser = (routes, isLoggedIn, role) => {
-	var result = null;
-	if (routes.length > 0) {
-		result = routes.map((route, index) => {
-			return (<Route
-				key={index}
-				path={route.path}
-				exact
-				render={props => (isLoggedIn) ? <route.main {...props} /> :
-					<Redirect to={{
-						pathname: '/login',
-						state: { from: props.location }
-					}} />}
-			/>)
-
-		})
-	}
-	return result;
-}
+const showRoutesPrivateUser = (routes, isLoggedIn, role) => {
+  var result = null;
+  if (routes.length > 0) {
+    result = routes.map((route, index) => {
+      return (
+        <Route
+          key={index}
+          path={route.path}
+          exact
+          render={(props) =>
+            isLoggedIn ? (
+              <route.main {...props} />
+            ) : (
+              <Redirect
+                to={{
+                  pathname: '/login',
+                  state: { from: props.location },
+                }}
+              />
+            )
+          }
+        />
+      );
+    });
+  }
+  return result;
+};
 
 export default App;
